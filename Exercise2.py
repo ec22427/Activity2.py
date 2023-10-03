ca8ab716e8db3edeefdcf712324800b8fe0f9dc3
ec22427 <ec22427@qmul.ac.uk>
2023-10-03 10:58:00 +0100

    (commit on 'main')

---
+++ Exercise2.py
@@ -0,0 +1,23 @@
+import unittest
+
+# The function to be tested
+def add_numbers(a,b)
+return a + b
+
+class TestFunction(unittest.Testcase):
+
+    def test_addition(self):
+        result = add_numbers(3, 5)
+        self.assertEqual(result, 8)
+
+    def test_addition_negative_numbers(self):
+         result = add_numbers(-2, 7)
+         self.assertEqual(result, 5)
+
+    def test_addition_float_numbers(self):
+        result = add_numbers(1.5, 2.5)
+        self.assertAlmostEqual(result, 4.0, places=2)
+
+if __name__ == '__main__':
+    unittest.main()
+    
